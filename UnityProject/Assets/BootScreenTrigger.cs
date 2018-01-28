ba903ab032e8e474048901c3fee91a581eedf341
Samuel Buzas <buzassg@mail.uc.edu>
2018-01-28 16:18:25 -0500

    God have mercy on my soul

---
+++ UnityProject/Assets/BootScreenTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BootScreenTrigger : MonoBehaviour {
+
+    public GameObject NewUIPanel;
+    public GameObject OldUIPanel;
+
+
+    private void OnEnable()
+    {
+        TextTyper.onBootDone += loadScreen;
+    }
+
+
+    void loadScreen()
+    {
+            OldUIPanel.SetActive(false);
+            NewUIPanel.SetActive(true);
+    }
+ 
+ 
+}
