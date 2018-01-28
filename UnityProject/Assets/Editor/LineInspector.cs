8e646054744aa3e86a4d9b37bc166066fc22d8b6
somn <somnacin7@gmail.com>
2018-01-27 22:13:44 -0500

    add inspector

---
+++ UnityProject/Assets/Editor/LineInspector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor
+{
+    private void OnSceneGUI()
+    {
+        Line line = target as Line;
+        Transform handleTransform = line.transform;
+        Vector3 p0 = handleTransform.TransformPoint(line.p0);
+        Vector3 p1 = handleTransform.TransformPoint(line.p1);
+
+        Handles.color = Color.white;
+        Handles.DrawLine(p0, p1);
+    }
+}
---
+++ UnityProject/Assets/Spline/Line.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Line : MonoBehaviour {
+
+    public Vector3 p0, p1;
+}
