1189c95b85a22682d8c698f4ab9996d666465a0a
Philip Wernersbach <philip.wernersbach@gmail.com>
2018-01-28 20:46:54 -0800

    Move hand and play sound when morse code is activated.

---
+++ UnityProject/Assets/Scripts/FrequencyListener.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrequencyListener : MonoBehaviour {
+	private const double LOWEST_FREQUENCY = -100;
+	private const double HIGHEST_FREQUENCY = 100;
+	private const double FREQUENCY_ADJUSTMENT = (HIGHEST_FREQUENCY - LOWEST_FREQUENCY) / 15;
+	private const double FREQUENCY_ADJUSTMENT_PER_SECOND = (HIGHEST_FREQUENCY - LOWEST_FREQUENCY) / 3;
+	private double targetFrequency = 0;
+	private double frequency = 0;
+
+	public HandMover handMover;
+
+	//public Button frequencyHigherButton;
+	//public Button frequencyLowerButton;
+
+	// Use this for initialization
+	void Start () {
+		//frequencyHigherButton.onClick.AddListener(onFrequencyAdjustHigher);
+		//frequencyLowerButton.onClick.AddListener(onFrequencyAdjustLower);
+	}
+
+	void UpdateTargetFrequency() {
+		bool increaseDecreaseDirection = (Random.value > 0.5f);
+		double adjustment = (increaseDecreaseDirection ? 1 : -1) * FREQUENCY_ADJUSTMENT_PER_SECOND * Time.deltaTime;
+
+		targetFrequency += adjustment;
+
+		if (targetFrequency < LOWEST_FREQUENCY)
+			targetFrequency = LOWEST_FREQUENCY;
+		else if (targetFrequency > HIGHEST_FREQUENCY)
+			targetFrequency = HIGHEST_FREQUENCY;
+	}
+
+	// Update is called once per frame
+	void UpdateCurrentFrequency () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			onFrequencyAdjustLower ();
+
+			handMover.moveRightHand (HandMover.Direction.Right);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			onFrequencyAdjustHigher ();
+
+			handMover.moveRightHand (HandMover.Direction.Left);
+		} else {
+			handMover.stopRightHand ();
+		}
+	}
+
+	void Update() {
+		UpdateTargetFrequency ();
+		UpdateCurrentFrequency ();
+
+		Debug.Log ("Target: " + targetFrequency);
+		Debug.Log ("Current: " + frequency);
+	}
+
+	void onFrequencyAdjustHigher() {
+		frequency += FREQUENCY_ADJUSTMENT * Time.deltaTime;
+
+		if (frequency > HIGHEST_FREQUENCY)
+			frequency = HIGHEST_FREQUENCY;
+	}
+
+	void onFrequencyAdjustLower() {
+		frequency -= FREQUENCY_ADJUSTMENT * Time.deltaTime;
+
+		if (frequency < LOWEST_FREQUENCY)
+			frequency = LOWEST_FREQUENCY;
+	}
+}
---
+++ UnityProject/Assets/Scripts/HandMover.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMover : MonoBehaviour {
+	public Animator leftHandMotion; 
+	public AudioSource leftHandSound;
+
+	public Animator rightHandMotion;
+
+	public enum Direction {None, Right, Left};
+
+	private Direction lastDirection = Direction.None;
+
+	public void moveLeftHand() {
+		leftHandMotion.Play ("Left Hand Only");
+		leftHandSound.Play ();
+	}
+
+	public void stopLeftHand() {
+		leftHandMotion.Play ("Idle Up");
+		leftHandSound.Stop ();
+	}
+
+	public void moveRightHand(Direction dir) {
+		if (dir != lastDirection) {
+			// stopRightHand ();
+
+			switch (dir) {
+			case Direction.Right:
+				moveRightHandRight ();
+				break;
+			case Direction.Left:
+				moveRightHandLeft ();
+				break;
+			}
+		}
+	}
+
+	void moveRightHandLeft() {
+		AnimatorStateInfo animatorStateInfo = rightHandMotion.GetCurrentAnimatorStateInfo (0);
+
+		if (animatorStateInfo.IsName("Idle"))
+			rightHandMotion.Play ("Center To Left");
+	}
+
+	void moveRightHandRight() {
+		AnimatorStateInfo animatorStateInfo = rightHandMotion.GetCurrentAnimatorStateInfo (0);
+
+		if (animatorStateInfo.IsName("Idle"))
+			rightHandMotion.Play ("Center To Right");
+	}
+
+	public void stopRightHand() {
+		AnimatorStateInfo animatorStateInfo = rightHandMotion.GetCurrentAnimatorStateInfo (0);
+
+		if (animatorStateInfo.IsName("Idle Left"))
+			rightHandMotion.Play ("Left to Center");
+		else if (animatorStateInfo.IsName("Idle Right"))
+			rightHandMotion.Play ("Right to Center");
+	}
+}
